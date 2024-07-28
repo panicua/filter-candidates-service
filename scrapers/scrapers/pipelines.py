5c1b4e7f6df4e2f61c9df35b35307159fc39bcb2
kekW <panicua12@gmail.com>
2024-07-28 04:30:38 +0300

    add: working single page candidates url dump to urls.json

--- scrapers/scrapers/pipelines.py
+++ scrapers/scrapers/pipelines.py
@@ -3,11 +3,37 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import pandas as pd
 
-class ScrapersPipeline:
+
+class JsonWriterPipeline:
+
+    def open_spider(self, spider):
+        self.items = []  # Initialize an empty list to store items
+
+    def close_spider(self, spider):
+        # Convert the list of items to a pandas DataFrame
+        df = pd.DataFrame(self.items)
+        # Write the DataFrame to a JSON file
+        df.to_json(
+            "urls.json",
+            orient="records",
+            lines=False,
+            force_ascii=False,
+            indent=4,
+        )
+
     def process_item(self, item, spider):
+        # Append each item to the list
+        self.items.append(dict(item))
         return item
+
+
+# class ScrapersPipeline:
+#     def process_item(self, item, spider):
+#         return item
--- scrapers/scrapers/spiders/work_ua.py
+++ scrapers/scrapers/spiders/work_ua.py
@@ -10,6 +10,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from scrapy import Item, Field
+
+
+class UrlItem(Item):
+    url = Field()
 
 
 # DON'T FORGET TO REMOVE COMMENTS
@@ -21,6 +26,9 @@ class WorkUaSpider(scrapy.Spider):
 
     def __init__(self) -> None:
         super().__init__()
+        # position will be set with telegram bot, later
+        self.position = "Python developer"
+        self.location = "Вся Україна"
         self.options = webdriver.ChromeOptions()
         # self.options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=self.options)
@@ -28,10 +36,11 @@ class WorkUaSpider(scrapy.Spider):
 
     def parse(self, response: Response, **kwargs) -> None:
         self.open_employer_page()
-        self.find_candidates_by_position("Python developer")
+        self.find_candidates_by_position(self.position, self.location)
         WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "a[href*='/resumes/']"))
+                (By.CSS_SELECTOR, "a[href*='/resumes/']")
+            )
         )
 
         # Extract candidate URLs
@@ -41,31 +50,39 @@ class WorkUaSpider(scrapy.Spider):
         for link in candidates_links:
             url = link.get_attribute("href")
             print(f"UUURL {url}")
-            # yield Request(url=response.urljoin(url), callback=self.parse_candidate)
+            yield UrlItem(url=url)
 
-        time.sleep(5)
+        # time.sleep(5)
 
-        self.driver.quit()
-
-        # while True:
-        #     break
-
-    def find_candidates_by_position(self, position: str) -> None:
-        position_field = self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Посада']")
+    def find_candidates_by_position(
+        self, position: str, location: str
+    ) -> None:
+        position_field = self.driver.find_element(
+            By.CSS_SELECTOR, "input[placeholder='Посада']"
+        )
         position_field.send_keys(position)
+        location_field = self.driver.find_element(
+            By.CSS_SELECTOR, "input[placeholder='Місто']"
+        )
+        location_field.send_keys(location)
 
-        search_button = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+        search_button = self.driver.find_element(
+            By.CSS_SELECTOR, "button[type='submit']"
+        )
         search_button.click()
+
         WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "a[href*='/resumes/']"))
+                (By.CSS_SELECTOR, "a[href*='/resumes/']")
+            )
         )
 
     def open_employer_page(self) -> None:
         self.driver.get(self.employer_url)
         WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "a[href*='/employer/']"))
+                (By.CSS_SELECTOR, "a[href*='/employer/']")
+            )
         )
 
     def close(self, reason) -> None:
@@ -77,5 +94,3 @@ if __name__ == "__main__":
     process = CrawlerProcess(settings)
     process.crawl(WorkUaSpider)
     process.start()
-
-

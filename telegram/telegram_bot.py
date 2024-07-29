0acf028e13550bc235af7c80ac20dd32eefacaa8
kekW <panicua12@gmail.com>
2024-07-29 20:04:01 +0300

    format,ref: used black formater, DTO refactored with dataclass

--- telegram/telegram_bot.py
+++ telegram/telegram_bot.py
@@ -1,8 +1,8 @@
 import logging
 import sys
 
-from dataclasses import dataclass, field
-from typing import List, Dict, Optional
+from dataclasses import dataclass
+from typing import Optional
 
 from decouple import config
 from telegram import (
@@ -10,7 +10,7 @@ from telegram import (
     ReplyKeyboardRemove,
     Update,
     InlineKeyboardButton,
-    InlineKeyboardMarkup
+    InlineKeyboardMarkup,
 )
 from telegram.ext import (
     Application,
@@ -19,7 +19,7 @@ from telegram.ext import (
     MessageHandler,
     filters,
     ContextTypes,
-    CallbackQueryHandler
+    CallbackQueryHandler,
 )
 
 from constants import LOCATION, EXPERIENCE, SALARY
@@ -33,18 +33,15 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+@dataclass
 class TelegramAnswersDTO:
-    def __init__(self, site,
-                 position, location,
-                 experience, salary,
-                 main_skills, secondary_skills):
-        self.site = site
-        self.position = position
-        self.location = location
-        self.experience = experience
-        self.salary = salary
-        self.main_skills = main_skills
-        self.secondary_skills = secondary_skills
+    site: Optional[str] = None
+    position: Optional[str] = None
+    location: Optional[str] = None
+    experience: Optional[str] = None
+    salary: Optional[str] = None
+    main_skills: Optional[str] = None
+    secondary_skills: Optional[str] = None
 
 
 class TelegramBot:
@@ -64,8 +61,7 @@ class TelegramBot:
             ["exit"],
         ]
 
-        self.answers = TelegramAnswersDTO(None, None, None, None, None, None,
-                                          None)
+        self.answers = TelegramAnswersDTO()
         self.validate_api_key_not_empty()
 
     def validate_api_key_not_empty(self) -> None:
@@ -77,9 +73,9 @@ class TelegramBot:
             )
             sys.exit(1)
 
-    async def start(self,
-                    update: Update,
-                    context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def start(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         await update.message.reply_text(
             "Hi! I'm the *Bot for candidates scraping*.\n"
             "Please choose a site to scrape.",
@@ -89,9 +85,9 @@ class TelegramBot:
         )
         return self.CHOOSING_OPTION
 
-    async def handle_user_choice(self,
-                                 update: Update,
-                                 context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def handle_user_choice(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         user_choice = update.message.text.lower()
 
         if user_choice in ["work.ua", "rabota.ua"]:
@@ -104,109 +100,117 @@ class TelegramBot:
             return self.ASKING_POSITION
 
         elif user_choice == "exit":
-            await update.message.reply_text("Bye!",
-                                            reply_markup=ReplyKeyboardRemove())
+            await update.message.reply_text(
+                "Bye!", reply_markup=ReplyKeyboardRemove()
+            )
             return ConversationHandler.END
 
         else:
             await update.message.reply_text(
-                "Invalid choice. Please select a valid option.")
+                "Invalid choice. Please select a valid option."
+            )
             return self.CHOOSING_OPTION
 
-    async def ask_position(self,
-                           update: Update,
-                           context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def ask_position(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         self.answers.position = update.message.text
         return await self.ask_location(update, context)
 
-    async def ask_location(self,
-                           update: Update,
-                           context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def ask_location(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         keyboard = [
-            [InlineKeyboardButton(loc, callback_data=loc) for loc in
-             list(LOCATION.keys())[i:i + 2]]
+            [
+                InlineKeyboardButton(loc, callback_data=loc)
+                for loc in list(LOCATION.keys())[i : i + 2]
+            ]
             for i in range(0, len(LOCATION.keys()), 2)
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        await update.message.reply_text("Please choose a location:",
-                                        reply_markup=reply_markup)
+        await update.message.reply_text(
+            "Please choose a location:", reply_markup=reply_markup
+        )
         return self.ASKING_LOCATION
 
-    async def location_selected(self,
-                                update: Update,
-                                context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def location_selected(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         query = update.callback_query
         await query.answer()
         self.answers.location = query.data
         return await self.ask_experience(update, context)
 
-    async def ask_experience(self,
-                             update: Update,
-                             context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def ask_experience(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         keyboard = [
-            [InlineKeyboardButton(exp, callback_data=exp) for exp in
-             list(EXPERIENCE.keys())[i:i + 2]]
+            [
+                InlineKeyboardButton(exp, callback_data=exp)
+                for exp in list(EXPERIENCE.keys())[i : i + 2]
+            ]
             for i in range(0, len(EXPERIENCE.keys()), 2)
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         await update.callback_query.message.reply_text(
-            "Please choose experience:", reply_markup=reply_markup)
+            "Please choose experience:", reply_markup=reply_markup
+        )
         return self.ASKING_EXPERIENCE
 
-    async def experience_selected(self,
-                                  update: Update,
-                                  context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def experience_selected(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         query = update.callback_query
         await query.answer()
         self.answers.experience = query.data
         return await self.ask_salary(update, context)
 
-    async def ask_salary(self,
-                         update: Update,
-                         context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def ask_salary(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         keyboard = [
-            [InlineKeyboardButton(salary, callback_data=salary) for salary in
-             list(SALARY.keys())[i:i + 2]]
+            [
+                InlineKeyboardButton(salary, callback_data=salary)
+                for salary in list(SALARY.keys())[i : i + 2]
+            ]
             for i in range(0, len(SALARY.keys()), 2)
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         await update.callback_query.message.reply_text(
-            "Please choose a salary range:", reply_markup=reply_markup)
+            "Please choose a salary range:", reply_markup=reply_markup
+        )
         return self.ASKING_SALARY
 
-    async def salary_selected(self,
-                              update: Update,
-                              context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def salary_selected(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         query = update.callback_query
         await query.answer()
         self.answers.salary = query.data
         await query.edit_message_text(
             text="Please enter the *MAIN* skills/keywords, "
-                 "separated by '*;*' and synonyms separated by '*,*':\n"
-                 "*Example:* python; drf, django rest framework; "
-                 "sql, postgresql, postgres;",
-            parse_mode="markdown"
+            "separated by '*;*' and synonyms separated by '*,*':\n"
+            "*Example:* python; drf, django rest framework; "
+            "sql, postgresql, postgres;",
+            parse_mode="markdown",
         )
         return self.ASKING_MAIN_SKILLS
 
-    async def main_skills_entered(self,
-                                  update: Update,
-                                  context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def main_skills_entered(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         self.answers.main_skills = update.message.text
-        # here should be secondary not above.
         await update.message.reply_text(
             text="Please enter the *SECONDARY* skills/keywords, "
-                 "separated by '*;*' and synonyms separated by '*,*':\n"
-                 "*Example:* aws, amazon web services, lambda; "
-                 "docker, docker-compose;",
+            "separated by '*;*' and synonyms separated by '*,*':\n"
+            "*Example:* aws, amazon web services, lambda; "
+            "docker, docker-compose;",
             parse_mode="markdown",
         )
         return self.ASKING_SECONDARY_SKILLS
 
     async def secondary_skills_entered(
-            self,
-            update: Update,
-            context: ContextTypes.DEFAULT_TYPE
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
     ) -> int:
         self.answers.secondary_skills = update.message.text
         await update.message.reply_text(
@@ -219,24 +223,27 @@ class TelegramBot:
             f"*Main Skills:* {self.answers.main_skills}\n"
             f"*Secondary Skills:* {self.answers.secondary_skills}\n\n"
             f"Type '*yes*' to confirm or '*no*' to abort.",
-            parse_mode="markdown"
+            parse_mode="markdown",
         )
         return self.CONFIRMING
 
-    async def confirm_details(self,
-                              update: Update,
-                              context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def confirm_details(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         if update.message.text.lower() in ["yes", "y"]:
             await update.message.reply_text("Starting the scraping process...")
-            # Here you can call the method to start the spider and fetch results.
+            #
+            # Here spider will be called and results returned.
+            #
             results = self.run_spider(self.answers)
             await update.message.reply_text(
-                "Scraping completed. Here are the results:\n{}".format(
-                    results))
+                "Scraping completed. Here are the results:\n{}".format(results)
+            )
             return ConversationHandler.END
         else:
             await update.message.reply_text(
-                "Process aborted. Restart with /start")
+                "Process aborted. Restart with /start"
+            )
             return ConversationHandler.END
 
     def run_spider(self, answers: TelegramAnswersDTO):
@@ -250,16 +257,17 @@ class TelegramBot:
             f"Experience: {answers.experience}\n",
             f"Salary: {answers.salary}\n",
             f"Main Skills: {answers.main_skills}\n",
-            f"Secondary Skills: {answers.secondary_skills}\n"
+            f"Secondary Skills: {answers.secondary_skills}\n",
         ]
 
     async def error_handler(
-            self, update: Update, context: ContextTypes.DEFAULT_TYPE
-    ):
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
         logger.error("Exception occurred:", exc_info=context.error)
 
-    async def exit(self, update: Update,
-                   context: ContextTypes.DEFAULT_TYPE) -> int:
+    async def exit(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> int:
         await update.message.reply_text("Conversation closed.")
         return ConversationHandler.END
 
@@ -271,26 +279,42 @@ class TelegramBot:
             entry_points=[CommandHandler("start", self.start)],
             states={
                 self.CHOOSING_OPTION: [
-                    MessageHandler(filters.TEXT & ~filters.COMMAND,
-                                   self.handle_user_choice)],
+                    MessageHandler(
+                        filters.TEXT & ~filters.COMMAND,
+                        self.handle_user_choice,
+                    )
+                ],
                 self.ASKING_POSITION: [
-                    MessageHandler(filters.TEXT & ~filters.COMMAND,
-                                   self.ask_position)],
+                    MessageHandler(
+                        filters.TEXT & ~filters.COMMAND, self.ask_position
+                    )
+                ],
                 self.ASKING_LOCATION: [
-                    CallbackQueryHandler(self.location_selected)],
+                    CallbackQueryHandler(self.location_selected)
+                ],
                 self.ASKING_EXPERIENCE: [
-                    CallbackQueryHandler(self.experience_selected)],
+                    CallbackQueryHandler(self.experience_selected)
+                ],
                 self.ASKING_SALARY: [
-                    CallbackQueryHandler(self.salary_selected)],
+                    CallbackQueryHandler(self.salary_selected)
+                ],
                 self.ASKING_MAIN_SKILLS: [
-                    MessageHandler(filters.TEXT & ~filters.COMMAND,
-                                   self.main_skills_entered)],
+                    MessageHandler(
+                        filters.TEXT & ~filters.COMMAND,
+                        self.main_skills_entered,
+                    )
+                ],
                 self.ASKING_SECONDARY_SKILLS: [
-                    MessageHandler(filters.TEXT & ~filters.COMMAND,
-                                   self.secondary_skills_entered)],
+                    MessageHandler(
+                        filters.TEXT & ~filters.COMMAND,
+                        self.secondary_skills_entered,
+                    )
+                ],
                 self.CONFIRMING: [
-                    MessageHandler(filters.TEXT & ~filters.COMMAND,
-                                   self.confirm_details)],
+                    MessageHandler(
+                        filters.TEXT & ~filters.COMMAND, self.confirm_details
+                    )
+                ],
             },
             fallbacks=[CommandHandler("exit", self.exit)],
         )
